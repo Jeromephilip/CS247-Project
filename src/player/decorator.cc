f3fca256de550bbad590d8e0ccf62fa4e8848892
jjebyphi <jjebyphi@uwaterloo.ca>
2023-08-01 16:27:24 -0400

    deleted decorator classes

--- src/player/decorator.cc
+++
@@ -1,5 +0,0 @@
-#include "decorator.h"
-
-Decorator::Decorator( string colour, bool isComputer, Computer *component ): Computer{colour, isComputer}, component{component} {}
-
-Decorator::~Decorator() { delete component; }
--- src/player/level4.cc
+++
@@ -1,6 +0,0 @@
-#include "level4.h"
-
-Move Level4::turnMove(int x, int y, int z, int w, Board& b) {
-    // return Move{0, 0, 0, 0, b.getPiece(0, 0)->isValidMove(0, 0, 0, 0), b.getPiece(0, 0)}; 
-    return Move{0, 0, 0, 0, b.getPiece(0, 0)->isValidMove(b, 0, 0, 0, 0), b.getPiece(0, 0)}; 
-} 
