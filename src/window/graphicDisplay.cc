5fb3200422164afd88dbdaf8731760eb3dfbe83d
mmawla <mmawla@uwaterloo.ca>
2023-08-01 09:39:55 -0400

    Changed board colours

--- src/window/graphicDisplay.cc
+++ src/window/graphicDisplay.cc
@@ -6,10 +6,10 @@ GraphicDisplay::GraphicDisplay(Game* game, Xwindow* window) : game{game}, window
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
             if ((i + j) % 2 == 0) {
-                window->fillRectangle(75 * j, i * 75, 75, 75, 0); 
+                window->fillRectangle(75 * j, i * 75, 75, 75, Xwindow::AntiqueWhite); 
             }
             else {
-                window->fillRectangle(75 * j, 75 * i, 75, 75, 3); 
+                window->fillRectangle(75 * j, 75 * i, 75, 75, Xwindow::DarkOliveGreen4); 
             }
         }
     }
@@ -36,10 +36,10 @@ void GraphicDisplay::notify() {
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
             if ((i + j) % 2 == 0) {
-                window->fillRectangle(75 * j, i * 75, 75, 75, 0); 
+                window->fillRectangle(75 * j, i * 75, 75, 75, Xwindow::AntiqueWhite); 
             }
             else {
-                window->fillRectangle(75 * j, 75 * i, 75, 75, 3); 
+                window->fillRectangle(75 * j, 75 * i, 75, 75, Xwindow::DarkOliveGreen4); 
             }
             if (game->getBoard().getPiece(j, i) != nullptr) {
                 window->drawString(75 * j + 37, 75 * i + 37, string(1, game->getBoard().getPiece(j, i)->getType())); 
